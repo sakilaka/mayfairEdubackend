5912dfad1d3721358e4f972cfb4e30e5f2f79606
jahid404 <jsjahidmini@gmail.com>
2024-10-04 04:11:28 +0000

    Oct 4, 2024, 10:11 AM

---
+++ database/migrations/2024_10_04_040942_create_page_controls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('page_controls', function (Blueprint $table) {
+            $table->id();
+            $table->string('page');
+            $table->string('page');
+            $table->enum('section', ['quick_links', 'explore', 'policies']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('page_controls');
+    }
+};
