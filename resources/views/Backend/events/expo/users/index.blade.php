221c070cf76d04f09dc2377375b3288bc4544b33
jahid404 <jsjahidmini@gmail.com>
2024-10-08 13:56:18 +0000

    Oct 8, 2024, 7:56 PM

--- resources/views/Backend/events/expo/users/index.blade.php
+++ resources/views/Backend/events/expo/users/index.blade.php
@@ -183,26 +183,33 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="participantModalLabel">Participant Details</h5>
+                                <h5 class="modal-title font-weight-bold" id="participantModalLabel">Participant
+                                    Details</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <p><strong>Ticket No:</strong> <span id="modalTicketNo"></span></p>
-                                <p><strong>Email:</strong> <span id="modalEmail"></span></p>
-                                <p><strong>First Name:</strong> <span id="modalFirstName"></span></p>
-                                <p><strong>Last Name:</strong> <span id="modalLastName"></span></p>
-                                <p><strong>ID Type:</strong> <span id="modalIdType"></span></p>
-                                <p><strong>ID No:</strong> <span id="modalIdNo"></span></p>
-                                <p><strong>Nationality:</strong> <span id="modalNationality"></span></p>
-                                <p><strong>Sex:</strong> <span id="modalSex"></span></p>
-                                <p><strong>Date of Birth:</strong> <span id="modalDob"></span></p>
-                                <p><strong>Phone:</strong> <span id="modalPhone"></span></p>
-                                <p><strong>Profession:</strong> <span id="modalProfession"></span></p>
-                                <p><strong>Institution:</strong> <span id="modalInstitution"></span></p>
-                                <p><strong>Program:</strong> <span id="modalProgram"></span></p>
-                                <p><strong>Degree:</strong> <span id="modalDegree"></span></p>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p style="font-size: 14px"><strong>Ticket No:</strong> <span id="modalTicketNo"></span></p>
+                                        <p style="font-size: 14px"><strong>Email:</strong> <span id="modalEmail"></span></p>
+                                        <p style="font-size: 14px"><strong>First Name:</strong> <span id="modalFirstName"></span></p>
+                                        <p style="font-size: 14px"><strong>Last Name:</strong> <span id="modalLastName"></span></p>
+                                        <p style="font-size: 14px"><strong>ID Type:</strong> <span id="modalIdType"></span></p>
+                                        <p style="font-size: 14px"><strong>ID No:</strong> <span id="modalIdNo"></span></p>
+                                        <p style="font-size: 14px"><strong>Nationality:</strong> <span id="modalNationality"></span></p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p style="font-size: 14px"><strong>Sex:</strong> <span id="modalSex"></span></p>
+                                        <p style="font-size: 14px"><strong>Date of Birth:</strong> <span id="modalDob"></span></p>
+                                        <p style="font-size: 14px"><strong>Phone:</strong> <span id="modalPhone"></span></p>
+                                        <p style="font-size: 14px"><strong>Profession:</strong> <span id="modalProfession"></span></p>
+                                        <p style="font-size: 14px"><strong>Institution:</strong> <span id="modalInstitution"></span></p>
+                                        <p style="font-size: 14px"><strong>Program:</strong> <span id="modalProgram"></span></p>
+                                        <p style="font-size: 14px"><strong>Degree:</strong> <span id="modalDegree"></span></p>
+                                    </div>
+                                </div>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -210,6 +217,7 @@
                         </div>
                     </div>
                 </div>
+
 
 
                 @include('Backend.components.footer')
