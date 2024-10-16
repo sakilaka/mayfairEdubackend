bbc2e4f5b7080c9f8a8ecdacd9cfee8178a66062
jahid404 <jsjahidmini@gmail.com>
2024-10-16 02:48:34 +0000

    Oct 16, 2024, 8:48 AM

--- resources/views/Expo/home_sections/hero.blade.php
+++ resources/views/Expo/home_sections/hero.blade.php
@@ -63,7 +63,14 @@
                 <div
                     class="d-flex flex-column flex-md-row mx-auto align-items-center justify-content-center p-2 px-4 border hero-bottom-location-container">
                     <div class="d-flex align-items-center me-md-3">
-                        <i class="bi bi-calendar2-event" style="color: #4CAF50;"></i>
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                            viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+                                clip-rule="evenodd" />
+                        </svg>
+
                         <span class="ms-2">21<sup>st</sup> & 22<sup>nd</sup> November, 2024</span>
                     </div>
                     <div class="d-flex align-items-center">
