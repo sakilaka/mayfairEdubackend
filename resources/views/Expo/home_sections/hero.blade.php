310b87d2995e7981936bc7872a2e1fcef1743c89
jahid404 <jsjahidmini@gmail.com>
2024-10-16 01:28:16 +0000

    Oct 16, 2024, 7:28 AM

--- resources/views/Expo/home_sections/hero.blade.php
+++ resources/views/Expo/home_sections/hero.blade.php
@@ -16,7 +16,7 @@
     <div class="bg-color"></div>
 
     <div style="width: 100%; height:100%;" class="d-flex justify-content-center align-items-center">
-        <div class="container row justify-content-center justify-content-md-start align-items-center mb-md-5 pb-md-5">
+        <div class="container row justify-content-center align-items-center mb-md-5 pb-md-5">
             <div class="col-md-10 p-4 pb-5" style="position: relative; z-index: 3;">
                 <div>
                     {{-- <h2 class="main-heading text-white fw-semibold">Biggest Expo In Bangladesh</h2> --}}
