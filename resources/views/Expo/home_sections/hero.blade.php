492e8b069679a4ef4c590508fe8cc3d3ba73a943
jahid404 <jsjahidmini@gmail.com>
2024-10-16 01:50:41 +0000

    Oct 16, 2024, 7:50 AM

--- resources/views/Expo/home_sections/hero.blade.php
+++ resources/views/Expo/home_sections/hero.blade.php
@@ -19,7 +19,7 @@
         <div class="container row justify-content-center justify-content-md-around align-items-center mb-md-5 pb-md-5">
             <div class="col-md-11 p-4 pb-5 pt-md-0" style="position: relative; z-index: 3;">
                 <div>
-                    <h2 class="main-heading text-white fw-semibold">Biggest Expo In Bangladesh</h2>
+                    <h2 class="main-heading text-white fw-bold">The 2nd</h2>
                     <h1 class="main-title text-white fw-bold">{{ $expo['title'] }}</h1>
                 </div>
                 <p class="location-text text-white">
