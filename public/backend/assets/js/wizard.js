2a5f22944f2eab1faa90d3f2d8eda0344134373e
jahid404 <jsjahidmini@gmail.com>
2024-10-16 04:57:55 +0000

    Oct 16, 2024, 10:57 AM

--- public/backend/assets/js/wizard.js
+++ public/backend/assets/js/wizard.js
@@ -9,6 +9,7 @@
       alert("Submit done!");
     }
   });
+  
   var validationForm = $("#example-validation-form");
   validationForm.val({
     errorPlacement: function errorPlacement(error, element) {
@@ -40,6 +41,7 @@
       alert("Submitted!");
     }
   });
+
   var verticalForm = $("#example-vertical-wizard");
   verticalForm.children("div").steps({
     headerTag: "h3",
