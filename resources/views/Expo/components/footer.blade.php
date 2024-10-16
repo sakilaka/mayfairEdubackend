1944334bdea3f36afb67e5d4010c7cb62c20622a
jahid404 <jsjahidmini@gmail.com>
2024-10-16 02:21:55 +0000

    Oct 16, 2024, 8:21 AM

--- resources/views/Expo/components/footer.blade.php
+++ resources/views/Expo/components/footer.blade.php
@@ -7,3 +7,68 @@
 <script>
     $('[data-toggle="tooltip"]').tooltip();
 </script>
+
+<style>
+    .fixed-buttons {
+        position: fixed;
+        bottom: 50%;
+        right: 0;
+        transform: translateY(50%);
+        z-index: 999;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        border-radius: 0 0 0 10px;
+        overflow: hidden;
+        transition: transform 0.3s ease;
+    }
+
+    .fixed-buttons.hidden {
+        transform: translateX(100%);
+    }
+
+    .fixed-buttons .btn {
+        width: 105px;
+        font-size: 12px;
+        font-weight: bold;
+        box-shadow: 0 4px 15px -5px rgba(50, 50, 50, 0.25);
+        border-radius: 0;
+    }
+
+    #btn-toggle {
+        position: fixed;
+        bottom: 50%;
+        right: 115px;
+        transform: translateY(50%);
+        z-index: 1000;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        font-size: 24px;
+        color: var(--primary_background);
+        transition: right 0.3s ease;
+    }
+</style>
+
+{{-- <button id="btn-toggle">
+        <i class="fa fa-arrow-left"></i>
+    </button> --}}
+
+<div class="fixed-buttons" id="fixed-buttons">
+    <a href="{{ route('frontend.university_course_list') }}" class="btn btn-tertiary-bg" id="btn-apply-now-fixed">Apply
+        Now</a>
+</div>
+<script>
+    document.getElementById('btn-toggle').addEventListener('click', function() {
+        const fixedButtons = document.getElementById('fixed-buttons');
+        const toggleButton = document.getElementById('btn-toggle');
+
+        fixedButtons.classList.toggle('hidden');
+
+        if (fixedButtons.classList.contains('hidden')) {
+            toggleButton.style.right = '5px';
+        } else {
+            toggleButton.style.right = '115px';
+        }
+    });
+</script>
