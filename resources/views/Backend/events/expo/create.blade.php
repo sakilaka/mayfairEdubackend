f5bcfbf2b2309b75f6d9bb99b82778c45e9d16dc
jahid404 <jsjahidmini@gmail.com>
2024-10-16 04:52:59 +0000

    Oct 16, 2024, 10:52 AM

--- resources/views/Backend/events/expo/create.blade.php
+++ resources/views/Backend/events/expo/create.blade.php
@@ -580,10 +580,12 @@
     @include('Backend.components.script')
     @include('Backend.components.ckeditor5-config')
 
+
     <script>
         $('.multipleSelect2Search').select2();
     </script>
 
+    <script src="{{ asset('backend/assets/js/wizard.js') }}"></script>
     <script src="{{ asset('backend/assets/js/dropify.js') }}"></script>
     <script>
         $(document).on('change', `.dropify`, function() {
