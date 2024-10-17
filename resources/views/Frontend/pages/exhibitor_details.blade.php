cfc6f9df8ed50bbcdca17b8fd602f6d3ce3f37ea
jahid404 <jsjahidmini@gmail.com>
2024-10-17 08:32:17 +0000

    Oct 17, 2024, 2:32 PM

--- resources/views/Frontend/pages/exhibitor_details.blade.php
+++ resources/views/Frontend/pages/exhibitor_details.blade.php
@@ -10,9 +10,10 @@
             <div class="container-lg">
                 <div class="row justify-content-center">
                     <div class="col-xl-12">
+                        <h2 class="h3 mb-4 mb-sm-5 text-center" style="font-weight: bold">{{ $exhibitor->name ?? '' }}</h2>
+
                         <div class="row mx-0 align-items-center justify-content-center border-md rounded-3">
                             <div class="col-md-10 p-4 p-sm-5">
-                                <h2 class="h3 mb-4 mb-sm-5" style="font-weight: bold">{{ $exhibitor->name ?? '' }}</h2>
                                 {!! $exhibitor->exhibitor_desc ?? '' !!}
                             </div>
                         </div>
