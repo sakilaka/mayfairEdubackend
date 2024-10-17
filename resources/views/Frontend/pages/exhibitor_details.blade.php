357a9ec0448f0338ee7407c86836b3d8d2aa79e4
jahid404 <jsjahidmini@gmail.com>
2024-10-17 13:14:34 +0000

    Oct 17, 2024, 7:14 PM

--- resources/views/Frontend/pages/exhibitor_details.blade.php
+++ resources/views/Frontend/pages/exhibitor_details.blade.php
@@ -12,8 +12,8 @@
                     <div class="col-xl-12">
                         <h2 class="h3 mb-4 mb-sm-5 text-center" style="font-weight: bold">{{ $exhibitor->name ?? '' }}</h2>
 
-                        <div class="row mx-0 align-items-center justify-content-center border-md rounded-3">
-                            <div class="col-md-10 p-4 p-sm-5 ckeditor5-rendered">
+                        <div class="row mx-0 align-items-center justify-content-center rounded-3">
+                            <div class="col-md-10 {{-- p-4 p-sm-5 --}} ckeditor5-rendered">
                                 @if ($exhibitor->exhibitor_desc)
                                     {!! $exhibitor->exhibitor_desc !!}
                                 @else
