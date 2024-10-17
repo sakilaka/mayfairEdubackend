ba07fdadabefb5ea50fb35d5774d0cd871a4198d
jahid404 <jsjahidmini@gmail.com>
2024-10-17 14:10:27 +0000

    Oct 17, 2024, 8:10 PM

--- app/Http/Controllers/Backend/Expo/ExpoController.php
+++ app/Http/Controllers/Backend/Expo/ExpoController.php
@@ -196,6 +196,7 @@ class ExpoController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        return $request->all();
         $validator = Validator::make($request->all(), [
             'title' => 'required',
             'date' => 'required',
