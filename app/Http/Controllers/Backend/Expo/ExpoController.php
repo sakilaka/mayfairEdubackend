224a6ef9c6a3620d2cb52981d3c35648a1e71b1a
jahid404 <jsjahidmini@gmail.com>
2024-10-17 14:16:50 +0000

    Oct 17, 2024, 8:16 PM

--- app/Http/Controllers/Backend/Expo/ExpoController.php
+++ app/Http/Controllers/Backend/Expo/ExpoController.php
@@ -420,7 +420,7 @@ class ExpoController extends Controller
                 }
 
                 $fileName = rand() . time() . '.' . $request->file('additional_contents.organizerDetails.logo')->getClientOriginalExtension();
-                $request->file('additional_contents.organizerDetails.logo')->move(public_path('upload/expo/'), $fileName);
+                // $request->file('additional_contents.organizerDetails.logo')->move(public_path('upload/expo/'), $fileName);
                 $data['additional_contents']['organizerDetails']['logo'] = url('upload/expo/' . $fileName);
             }
 
@@ -434,7 +434,7 @@ class ExpoController extends Controller
                 }
 
                 $fileName = rand() . time() . '.' . $request->file('additional_contents.co_organizerDetails.logo')->getClientOriginalExtension();
-                $request->file('additional_contents.co_organizerDetails.logo')->move(public_path('upload/expo/'), $fileName);
+                // $request->file('additional_contents.co_organizerDetails.logo')->move(public_path('upload/expo/'), $fileName);
                 $data['additional_contents']['co_organizerDetails']['logo'] = url('upload/expo/' . $fileName);
             }
             return $data['additional_contents'];
