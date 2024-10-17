8e971fe62a466d7adb9d25929b0fcc2d34797245
jahid404 <jsjahidmini@gmail.com>
2024-10-17 17:07:49 +0000

    Oct 17, 2024, 11:07 PM

--- resources/views/Expo/details.blade.php
+++ resources/views/Expo/details.blade.php
@@ -4,16 +4,30 @@
 <head>
     @include('Expo.components.head')
     <title>{{ env('APP_NAME') }} - Details of {{ $expo['title'] }}</title>
-
     <link rel="stylesheet" href="{{ asset('frontend/ckeditor5-rendered.css') }}">
-</head>
 
-<body>
     @php
         $additional_contents = json_decode($expo->additional_contents, true) ?? [];
         $place = json_decode($expo->place, true) ?? [];
         $datetime = json_decode($expo->datetime, true) ?? [];
     @endphp
+
+    @if ($additional_contents['hero_bg'])
+        <style>
+            .bg-section {
+                background-image: url('{{ $additional_contents['hero_bg'] }}');
+            }
+        </style>
+    @else
+        <style>
+            .bg-section {
+                background-color: var(--primary_background);
+            }
+        </style>
+    @endif
+</head>
+
+<body>
 
     @include('Expo.home_sections.hero')
 
