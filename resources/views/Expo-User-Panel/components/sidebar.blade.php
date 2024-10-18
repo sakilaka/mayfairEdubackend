1c637ec285257efc2df7fd41faa3cedf8d180f71
jahid404 <jsjahidmini@gmail.com>
2024-10-18 17:18:48 +0000

    Oct 18, 2024, 11:18 PM

--- resources/views/Expo-User-Panel/components/sidebar.blade.php
+++ resources/views/Expo-User-Panel/components/sidebar.blade.php
@@ -1,17 +1,17 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         {{-- dashboard --}}
-        <li class="nav-item {{ Route::is('user.dashboard') || Route::is('user.profile') ? 'active' : '' }}">
-            <a class="nav-link {{ Route::is('user.dashboard') || Route::is('user.profile') ? 'active' : '' }}"
-                href="{{ route('user.dashboard') }}">
+        <li class="nav-item {{ Route::is('expo.user.dashboard') || Route::is('expo.user.profile') ? 'active' : '' }}">
+            <a class="nav-link {{ Route::is('expo.user.dashboard') || Route::is('expo.user.profile') ? 'active' : '' }}"
+                href="{{ route('expo.user.dashboard') }}">
                 <i class="fa fa-cogs menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
 
         {{-- dashboard --}}
-        <li class="nav-item {{ Route::is('user.my_tickets') ? 'active' : '' }}">
-            <a class="nav-link {{ Route::is('user.my_tickets') ? 'active' : '' }}" href="{{ route('user.my_tickets') }}">
+        <li class="nav-item {{ Route::is('expo.user.my_tickets') ? 'active' : '' }}">
+            <a class="nav-link {{ Route::is('expo.user.my_tickets') ? 'active' : '' }}" href="{{ route('expo.user.my_tickets') }}">
                 <i class="fa fa-address-book menu-icon"></i>
                 <span class="menu-title">Tickets</span>
             </a>
