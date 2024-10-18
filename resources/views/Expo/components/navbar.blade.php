62ad3fb047decc1d4ea6fdb63f532621b685b252
jahid404 <jsjahidmini@gmail.com>
2024-10-18 06:40:25 +0000

    Oct 18, 2024, 12:40 PM

--- resources/views/Expo/components/navbar.blade.php
+++ resources/views/Expo/components/navbar.blade.php
@@ -17,7 +17,7 @@
             <a class="nav-link" href="http://studyinchinaexhibition.com/exhibitors" style="color: white;">Delegates</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('expo.testimonials', ['unique_id' => $expo->unique_id]) }}s"
+            <a class="nav-link" href="{{ route('expo.testimonials', ['unique_id' => $expo->unique_id]) }}"
                 style="color: white;">Testimonial</a>
         </li>
         <li class="nav-item">
---
+++ resources/views/Expo/pages/testimonials.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('Expo.components.head')
+    <title>{{ env('APP_NAME') }} - Testimonials of '{{ $expo->title }}'</title>
+    <link rel="stylesheet" href="{{ asset('frontend/ckeditor5-rendered.css') }}">
+
+    @php
+        $additional_contents = json_decode($expo->additional_contents, true) ?? [];
+    @endphp
+</head>
+
+<body>
+
+    <div class="bg-section" style="height:auto;">
+        <div class="container">
+            <nav class="navbar navbar-expand-md shadow-none" style="z-index: 3">
+                <div class="container d-flex justify-content-between">
+                    <a class="navbar-brand" href="{{ route('expo.details', ['id' => $expo->unique_id]) }}">
+                        <img src="{{ $additional_contents['nav_logo'] }}" alt="Logo" class="logo">
+                    </a>
+
+                    @include('Expo.components.navbar')
+                </div>
+            </nav>
+        </div>
+
+        {{-- <div class="layer-image"></div> --}}
+        <div class="bg-color"></div>
+    </div>
+
+    <section class="university-highlights my-5">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-title">Schedule</h2>
+            </div>
+
+            <div class="row justify-content-center mt-5">
+                
+            </div>
+
+        </div>
+    </section>
+
+    @include('Expo.home_sections.footer')
+    @include('Expo.components.footer')
+</body>
+
+</html>
