d29d6cdd629211050f6209c3809a767155f34917
jahid404 <jsjahidmini@gmail.com>
2024-10-18 15:17:06 +0000

    Oct 18, 2024, 9:17 PM

--- resources/views/Expo/components/navbar.blade.php
+++ resources/views/Expo/components/navbar.blade.php
@@ -1,29 +1,32 @@
 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
     <ul class="navbar-nav ms-auto d-flex align-items-center custom-navbar-width">
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('expo.details', ['id' => $expo->unique_id]) }}"
-                style="color: white;">Home</a>
+            <a class="nav-link {{ Route::is('expo.details') ? 'active' : '' }}"
+                href="{{ route('expo.details', ['id' => $expo->unique_id]) }}" style="color: white;">Home</a>
         </li>
 
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('expo.schedule', ['unique_id' => $expo->unique_id]) }}"
-                style="color: white;">Schedule</a>
+            <a class="nav-link {{ Route::is('expo.schedule') ? 'active' : '' }}"
+                href="{{ route('expo.schedule', ['unique_id' => $expo->unique_id]) }}" style="color: white;">Schedule</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('expo.exhibitors', ['unique_id' => $expo->unique_id]) }}"
+            <a class="nav-link {{ Route::is('expo.exhibitors') ? 'active' : '' }}"
+                href="{{ route('expo.exhibitors', ['unique_id' => $expo->unique_id]) }}"
                 style="color: white;">Exhibitors</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('expo.delegates', ['unique_id' => $expo->unique_id]) }}"
+            <a class="nav-link {{ Route::is('expo.delegates') ? 'active' : '' }}"
+                href="{{ route('expo.delegates', ['unique_id' => $expo->unique_id]) }}"
                 style="color: white;">Delegates</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('expo.testimonials', ['unique_id' => $expo->unique_id]) }}"
+            <a class="nav-link {{ Route::is('expo.testimonials') ? 'active' : '' }}"
+                href="{{ route('expo.testimonials', ['unique_id' => $expo->unique_id]) }}"
                 style="color: white;">Testimonial</a>
         </li>
         <li class="nav-item nav-profile dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="whyChinaDropdown"
-                aria-expanded="false">
+            <a class="nav-link dropdown-toggle {{ Route::is('expo.gallery') || Route::is('expo.video') ? 'active' : '' }} text-white"
+                href="#" data-toggle="dropdown" id="whyChinaDropdown" aria-expanded="false">
                 <span class="mr-2">Media</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown shadow" aria-labelledby="whyChinaDropdown">
