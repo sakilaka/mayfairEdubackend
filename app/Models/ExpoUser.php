f2a6586836e84fa6291567a18a686c3abaaa746e
jahid404 <jsjahidmini@gmail.com>
2024-10-18 15:49:58 +0000

    Oct 18, 2024, 9:49 PM

---
+++ app/Models/ExpoUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpoUser extends Model
+{
+    use HasFactory;
+    protected $table = 'expo_users';
+}
