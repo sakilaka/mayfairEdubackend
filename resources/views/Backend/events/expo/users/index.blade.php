2af5a1bf357b670a988c73c8f1b53afa7c1ba8a4
jahid404 <jsjahidmini@gmail.com>
2024-10-18 18:43:32 +0000

    Oct 19, 2024, 12:43 AM

--- resources/views/Backend/events/expo/users/index.blade.php
+++ resources/views/Backend/events/expo/users/index.blade.php
@@ -338,7 +338,7 @@
                     $('#modalProgram').text(response.program);
                     $('#modalDegree').text(response.degree);
                 },
-                error: function(xhr) {
+                error: function(xhr, ) {
                     alert('Failed to fetch participant data. Please try again.');
                 }
             });
