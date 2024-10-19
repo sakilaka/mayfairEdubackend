2a6bfecf9a796e273fd106aff155bec455cf6903
jahid404 <jsjahidmini@gmail.com>
2024-10-19 10:34:06 +0000

    Oct 19, 2024, 4:34 PM

--- resources/views/Backend/events/expo/main/join/index.blade.php
+++ resources/views/Backend/events/expo/main/join/index.blade.php
@@ -67,21 +67,22 @@
                                                 </div>
                                             </div>
                                             <div class="col-12">
-                                                <div
-                                                    class="row justify-content-between align-items-center steps-container">
-                                                    <div class="col-11 px-3 mb-2">
-                                                        <label class="form-label">
-                                                            Step Title
-                                                            <span class="text-danger">*</span>
-                                                        </label>
-                                                        <input type="text" class="form-control" name=""
-                                                            placeholder="Enter Step Title" value="" required>
-                                                    </div>
-                                                    <div class="col-1 mt-3">
-                                                        <a href="javascript:void(0)"
-                                                            class="btn btn-sm btn-danger remove-step">
-                                                            <i class="fa fa-minus"></i>
-                                                        </a>
+                                                <div class="steps-container">
+                                                    <div class="row justify-content-between align-items-center step-row">
+                                                        <div class="col-11 px-3 mb-2">
+                                                            <label class="form-label">
+                                                                Step Title
+                                                                <span class="text-danger">*</span>
+                                                            </label>
+                                                            <input type="text" class="form-control" name=""
+                                                                placeholder="Enter Step Title" value="" required>
+                                                        </div>
+                                                        <div class="col-1 mt-3">
+                                                            <a href="javascript:void(0)"
+                                                                class="btn btn-sm btn-danger remove-step">
+                                                                <i class="fa fa-minus"></i>
+                                                            </a>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -741,10 +742,12 @@
                         Step Title
                         <span class="text-danger">*</span>
                     </label>
-                    <input type="text" class="form-control" name="step_titles[]" placeholder="Enter Step Title" required>
+                    <input type="text" class="form-control" name=""
+                        placeholder="Enter Step Title" value="" required>
                 </div>
                 <div class="col-1 mt-3">
-                    <a href="javascript:void(0)" class="btn btn-sm btn-danger remove-step">
+                    <a href="javascript:void(0)"
+                        class="btn btn-sm btn-danger remove-step">
                         <i class="fa fa-minus"></i>
                     </a>
                 </div>
