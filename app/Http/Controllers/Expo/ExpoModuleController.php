976504aff12e3637aa29d5f110f8d879a5cd2aa6
jahid404 <jsjahidmini@gmail.com>
2024-10-19 06:10:32 +0000

    Oct 19, 2024, 12:10 PM

--- app/Http/Controllers/Expo/ExpoModuleController.php
+++ app/Http/Controllers/Expo/ExpoModuleController.php
@@ -43,7 +43,7 @@ class ExpoModuleController extends Controller
 
         $data['exhibitors'] = $universities->map(function ($university) use ($exhibitor_data) {
             $exhibitor_info = collect($exhibitor_data)->firstWhere('exhibitor', $university->id);
-            $university->show_on_home = $exhibitor_info['show_on_home'] ?? false;
+            $university->show_in_expo = $exhibitor_info['show_in_expo'] ?? false;
             $university->position_in_expo = $exhibitor_info['position_in_expo'] ?? null;
             return $university;
         })->sortBy(function ($university) {
