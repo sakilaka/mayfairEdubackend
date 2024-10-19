a6af371294581340c53766ed3f18ae4805f506d9
jahid404 <jsjahidmini@gmail.com>
2024-10-19 04:59:23 +0000

    Oct 19, 2024, 10:59 AM

--- app/Http/Controllers/Backend/Expo/Main/ExpoMainExhibitorController.php
+++ app/Http/Controllers/Backend/Expo/Main/ExpoMainExhibitorController.php
@@ -89,7 +89,6 @@ class ExpoMainExhibitorController extends Controller
             }
 
             $expo->exhibitors = json_encode($exhibitors);
-            return $expo;
             $expo->save();
 
             return back()->with('success', 'Selected universities have been marked as exhibitors!');
