4f55ba7d6e4263493543e5f318c8d7f2c45e70b6
jahid404 <jsjahidmini@gmail.com>
2024-10-19 13:28:09 +0000

    Oct 19, 2024, 7:28 PM

--- app/Http/Controllers/Backend/Expo/Main/ExpoMainExhibitorController.php
+++ app/Http/Controllers/Backend/Expo/Main/ExpoMainExhibitorController.php
@@ -70,14 +70,13 @@ class ExpoMainExhibitorController extends Controller
      */
     public function exhibitor_update(Request $request, $expo_id, $exhibitor_id)
     {
-        $exhibitor = University::where('id', $exhibitor_id)->first();
+        $exhibitor = University::find($exhibitor_id);
 
         if (!$exhibitor) {
             return back()->with('error', 'Exhibitor Not Found!');
         }
 
         $exhibitor->exhibitor_desc = $request->description;
-        return $exhibitor;
         $exhibitor->save();
 
         return redirect(route('admin.expo.exhibitors.index', ['expo_id' => $expo_id]))->with('success', 'Exhibitor Description Has Been Updated!');
