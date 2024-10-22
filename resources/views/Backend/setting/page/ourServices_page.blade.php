62344d999db8f3bb692179ed9862433f65e9ae31
jahid404 <jsjahidmini@gmail.com>
2024-10-22 07:28:48 +0000

    Oct 22, 2024, 1:28 PM

--- resources/views/Backend/setting/page/ourServices_page.blade.php
+++ resources/views/Backend/setting/page/ourServices_page.blade.php
@@ -327,7 +327,7 @@
         $('#add-services-large').on('click', function() {
             var randomNumber = Math.floor(10000 + Math.random() * 90000);
 
-            const editorKey = 'service_large_long_description[]';
+            const editorKey = 'service_large_long_descriptio[]';
             if (!window.editorInstances || !window.editorInstances[editorKey]) {
                 console.error(`Editor instance for '${editorKey}' not found.`);
             } else {
@@ -419,7 +419,7 @@
                                 Description
                                 <span class="text-danger">*</span>
                             </label>
-                            <textarea name="service_large_long_description[]" class="form-control editor"></textarea>
+                            <textarea name="service_large_long_descriptio[]" class="form-control editor"></textarea>
                         </div>
                     </div>
 
