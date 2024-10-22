e094a84b1bb0618d4cf066aee69c0ead328d4f8a
jahid404 <jsjahidmini@gmail.com>
2024-10-22 04:27:11 +0000

    Oct 22, 2024, 10:27 AM

--- app/Exports/ExpoUsersExport.php
+++ app/Exports/ExpoUsersExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\ExpoUser;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExpoUsersExport implements FromCollection
+class ExpoUsersExport implements FromCollection, WithHeadings
 {
     protected $expoUser;
 
@@ -17,5 +18,24 @@ class ExpoUsersExport implements FromCollection
     public function collection()
     {
         return $this->expoUser;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Ticket Number',
+            'Expo ID',
+            'Email',
+            'First Name',
+            'Last Name',
+            'Nationality',
+            'Sex',
+            'Date of Birth',
+            'Phone',
+            'Profession',
+            'Institution',
+            'Program',
+            'Degree',
+        ];
     }
 }
