f57ddae4e62cbdc7daf4d67fec7d2e5e41cc0ce0
jahid404 <jsjahidmini@gmail.com>
2024-10-24 15:31:27 +0000

    Oct 24, 2024, 9:31 PM

--- resources/views/Expo/home_sections/footer.blade.php
+++ resources/views/Expo/home_sections/footer.blade.php
@@ -1,7 +1,6 @@
 @php
     $additional_contents = json_decode($expo->additional_contents, true) ?? [];
     $footer_contents = json_decode($expo->footer_contents, true) ?? [];
-    dd($footer_contents);
 @endphp
 <section class="red-section-bg py-5">
     <div class="container">
@@ -78,33 +77,6 @@
 
                 <div class="col-12">
                     <div class="d-flex flex-wrap justify-content-center">
-                        <div class="footer_social_item">
-                            <div class="footer_social_icons_container"
-                                onclick="openInNewTab('https://web.facebook.com/chinaalumni/')" style="margin-left: 0"
-                                data-toggle="tooltip" title="China Alumni Association">
-                                <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                    viewBox="0 0 24 24">
-                                    <path fill-rule="evenodd"
-                                        d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="footer_social_item">
-                            <div class="footer_social_icons_container"
-                                onclick="openInNewTab('https://www.instagram.com/chinaalumni/')" data-toggle="tooltip"
-                                title="China Alumni">
-                                <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path fill="currentColor" fill-rule="evenodd"
-                                        d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                            </div>
-                        </div>
-
                         <div class="footer_social_item">
                             <div class="footer_social_icons_container" onclick="openInNewTab('')">
                                 <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
