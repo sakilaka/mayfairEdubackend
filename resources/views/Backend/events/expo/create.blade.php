8318eca21d5bebc4b3aaf6caa8ff6164860752b5
jahid404 <jsjahidmini@gmail.com>
2024-10-24 14:13:58 +0000

    Oct 24, 2024, 8:13 PM

--- resources/views/Backend/events/expo/create.blade.php
+++ resources/views/Backend/events/expo/create.blade.php
@@ -987,11 +987,14 @@
                                                                 </div>
                                                             </div>
                                                             <div class="col-md-1">
-                                                                <a href="javascript:void(0)"
-                                                                    class="btn btn-danger btn-sm m-0 ml-2">
-                                                                    <i class="fas fa-minus-circle">
-                                                                    </i>
-                                                                </a>
+                                                                <div class="form-group">
+                                                                    <label>&nbsp;</label>
+                                                                    <a href="javascript:void(0)"
+                                                                        class="btn btn-danger btn-sm m-0 ml-2">
+                                                                        <i class="fas fa-minus-circle">
+                                                                        </i>
+                                                                    </a>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
