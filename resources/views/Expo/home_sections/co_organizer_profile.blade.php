3a595ca7cd1289e63d93b10773d03d5bc342de7e
jahid404 <jsjahidmini@gmail.com>
2024-10-26 17:15:38 +0000

    Oct 26, 2024, 11:15 PM

--- resources/views/Expo/home_sections/co_organizer_profile.blade.php
+++ resources/views/Expo/home_sections/co_organizer_profile.blade.php
@@ -4,7 +4,7 @@
             <h2 class="section-title">Co-Organizer</h2>
         </div>
 
-        @foreach ($additional_contents['co_organizerDetails'] ?? [] as $co_organizer)
+        @forelse ($additional_contents['co_organizerDetails'] ?? [] as $co_organizer)
             <div class="row justify-content-center mt-5">
                 <div class="col-md-3">
                     <div class="d-flex flex-column justify-content-center align-items-center align-items-md-end">
@@ -28,6 +28,6 @@
                 </div>
                 <div class="col-md-1"></div>
             </div>
-        @endforeach
+        @endforelse
     </div>
 </section>
