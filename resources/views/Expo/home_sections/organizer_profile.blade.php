ee9c720664b063d1798d94390c556db1cdf1a251
jahid404 <jsjahidmini@gmail.com>
2024-10-26 17:14:17 +0000

    Oct 26, 2024, 11:14 PM

--- resources/views/Expo/home_sections/organizer_profile.blade.php
+++ resources/views/Expo/home_sections/organizer_profile.blade.php
@@ -4,12 +4,11 @@
             <h2 class="section-title text-light">Organizer</h2>
         </div>
 
-        @foreach ($additional_contents['organizerDetails'] as $organizer)
+        @foreach ($additional_contents['organizerDetails'] ?? [] as $organizer)
             <div class="row justify-content-center mt-5">
                 <div class="col-md-3">
                     <div class="d-flex flex-column justify-content-center align-items-center align-items-md-end">
-                        <img src="{{ $organizer['logo'] ?? '' }}" alt=""
-                            class="img-fluid" width="150">
+                        <img src="{{ $organizer['logo'] ?? '' }}" alt="" class="img-fluid" width="150">
                         <div class="text-center mt-4 me-md-3">
                             <a href="{{ $organizer['redirect_url'] ?? '#' }}"
                                 class="btn btn-light mx-auto px-5 rounded-0" target="_blank">Details</a>
