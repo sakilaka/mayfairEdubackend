8f6784a213f72c5a2c83fb5b4d7edc2ac381fe87
jahid404 <jsjahidmini@gmail.com>
2024-10-26 05:54:52 +0000

    Oct 26, 2024, 11:54 AM

--- resources/views/Expo/home_sections/hero.blade.php
+++ resources/views/Expo/home_sections/hero.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg shadow-none" style="z-index: 3">
             <div class="container d-flex justify-content-between">
-                <a class="ms-md-4 navbar-brand" href="{{ route('home') }}">
+                <a class="ms-md-4 ps-md-4 navbar-brand" href="{{ route('home') }}">
                     <img src="{{ $additional_contents['nav_logo'] ?? '' }}" alt="Logo"
                         class="logo" style="width: 180px; height:auto;">
                 </a>
