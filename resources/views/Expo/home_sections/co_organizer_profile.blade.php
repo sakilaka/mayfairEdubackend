9c736d5a37ac4cc2765aeabae8ddd4312856a68d
jahid404 <jsjahidmini@gmail.com>
2024-10-26 16:45:41 +0000

    Oct 26, 2024, 10:45 PM

--- resources/views/Expo/home_sections/co_organizer_profile.blade.php
+++ resources/views/Expo/home_sections/co_organizer_profile.blade.php
@@ -4,28 +4,30 @@
             <h2 class="section-title">Co-Organizer</h2>
         </div>
 
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-3">
-                <div class="d-flex flex-column justify-content-center align-items-center align-items-md-end">
-                    <img src="{{ $additional_contents['co_organizerDetails']['logo'] ?? '' }}" alt=""
-                        class="img-fluid" width="150">
-                    <div class="text-center mt-4 me-md-3">
-                        <a href="{{ $additional_contents['co_organizerDetails']['redirect_url'] ?? '#' }}" class="btn btn-primary-bg red-hover-button mx-auto px-5 rounded-0"
-                            target="_blank">Details</a>
+        @foreach ($additional_contents['co_organizerDetails'] as $co_organizer)
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center align-items-md-end">
+                        <img src="{{ $co_organizer['logo'] ?? '' }}" alt="" class="img-fluid" width="150">
+                        <div class="text-center mt-4 me-md-3">
+                            <a href="{{ $co_organizer['redirect_url'] ?? '#' }}"
+                                class="btn btn-primary-bg red-hover-button mx-auto px-5 rounded-0"
+                                target="_blank">Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-8 mt-4 mt-md-0">
-                <p class="fs-5 mb-2">
-                    <strong class="fw700">{{ $additional_contents['co_organizerDetails']['name'] ?? '' }}</strong>
-                </p>
+                <div class="col-md-8 mt-4 mt-md-0">
+                    <p class="fs-5 mb-2">
+                        <strong class="fw700">{{ $co_organizer['name'] ?? '' }}</strong>
+                    </p>
 
-                <div class="ckeditor5-rendered">
-                    {!! $additional_contents['co_organizerDetails']['details'] ?? '' !!}
+                    <div class="ckeditor5-rendered">
+                        {!! $co_organizer['details'] ?? '' !!}
+                    </div>
                 </div>
+                <div class="col-md-1"></div>
             </div>
-            <div class="col-md-1"></div>
-        </div>
+        @endforeach
     </div>
 </section>
--- resources/views/Expo/home_sections/organizer_profile.blade.php
+++ resources/views/Expo/home_sections/organizer_profile.blade.php
@@ -4,28 +4,30 @@
             <h2 class="section-title text-light">Organizer</h2>
         </div>
 
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-3">
-                <div class="d-flex flex-column justify-content-center align-items-center align-items-md-end">
-                    <img src="{{ $additional_contents['organizerDetails']['logo'] ?? '' }}" alt=""
-                        class="img-fluid" width="150">
-                    <div class="text-center mt-4 me-md-3">
-                        <a href="{{ $additional_contents['organizerDetails']['redirect_url'] ?? '#' }}"
-                            class="btn btn-light mx-auto px-5 rounded-0" target="_blank">Details</a>
+        @foreach ($additional_contents['organizerDetails'] as $organizer)
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-3">
+                    <div class="d-flex flex-column justify-content-center align-items-center align-items-md-end">
+                        <img src="{{ $organizer['logo'] ?? '' }}" alt=""
+                            class="img-fluid" width="150">
+                        <div class="text-center mt-4 me-md-3">
+                            <a href="{{ $organizer['redirect_url'] ?? '#' }}"
+                                class="btn btn-light mx-auto px-5 rounded-0" target="_blank">Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-8 mt-4 mt-md-0">
-                <p class="text-light fs-5 mb-2">
-                    <strong class="fw700">{{ $additional_contents['organizerDetails']['name'] ?? '' }}</strong>
-                </p>
+                <div class="col-md-8 mt-4 mt-md-0">
+                    <p class="text-light fs-5 mb-2">
+                        <strong class="fw700">{{ $organizer['name'] ?? '' }}</strong>
+                    </p>
 
-                <div class="ckeditor5-rendered text-light">
-                    {!! $additional_contents['organizerDetails']['details'] ?? '' !!}
+                    <div class="ckeditor5-rendered text-light">
+                        {!! $organizer['details'] ?? '' !!}
+                    </div>
                 </div>
+                <div class="col-md-1"></div>
             </div>
-            <div class="col-md-1"></div>
-        </div>
+        @endforeach
     </div>
 </section>
