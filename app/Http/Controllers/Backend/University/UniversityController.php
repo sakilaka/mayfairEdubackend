614938403d6ff1b6b2a68b059ac270ea1726627e
jahid404 <jsjahidmini@gmail.com>
2024-11-10 05:06:58 +0000

    Nov 10, 2024, 11:06 AM

--- app/Http/Controllers/Backend/University/UniversityController.php
+++ app/Http/Controllers/Backend/University/UniversityController.php
@@ -136,7 +136,7 @@ class UniversityController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|',
+            'name' => 'required|max:5',
         ]);
 
         try {
